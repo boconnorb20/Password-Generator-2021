a4634d4604f4aa3270b6a3992ec1213567b572c4
boconnorb20 <80371646+boconnorb20@users.noreply.github.com>
2021-05-19 12:36:49 -0500

    changes to script file, working on next steps in AC's

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Creating variables for the password criteria
+var length = 0;
+var charTypes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+var charSymbols = ("!@#$%^&*?");
 
-// Add event listener to generate button
+// Generate password length
+function generatePasswordLength() {
+ 
+  length = prompt("How long do you want your password? Password MUST be between 8 and 128 characters");
+  if (length < 8 || length > 128){
+    alert("NO PASSWORD FOR YOU");
+    return null;
+ }
+ return length;
+} 
+
+function generateCharType() {
+
+  charTypes = prompt("Password must contain Uppercase, Lowercase and Numerical characters, Still between 8 and 128 Characters");
+}
+
+
+
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
