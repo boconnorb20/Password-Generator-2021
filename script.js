4d37f785a49dd7e9623412cee39fb396ccbd67c0
Brian OConnor <boconnor20@gmail.com>
2021-05-20 17:56:34 -0500

    additions to js file

--- script.js
+++ script.js
@@ -1,4 +1,79 @@
 
+
+// Creating variables for the password criteria
+var length = 0;
+var charTypes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+var charSymbols = ("!@#$%^&*?");
+
+
+// Generate password length
+function generatePasswordChoice() {
+ 
+  length = prompt("How long do you want your password? Password MUST be between 8 and 128 characters");
+  if (length < 8 || length > 128){
+    alert("NO PASSWORD FOR YOU");
+    return null;
+  }
+ //  return length;
+ var lower = confirm("click ok if you to include lowercase in pw");
+ var upper = confirm("click ok if you to include uppercase in pw");
+ var symbols = confirm("click ok if you to include symbols in pw");
+ var numbers = confirm("click ok if you to include numbers in pw");
+ if(lower === false && upper === false && symbols === false && numbers === false) {
+   alert("TRY AGAIN")
+   return;
+ }
+ var passwordChoice = {
+   length:length, 
+   lower:lower,
+   upper:upper,
+   symbols:symbols,
+   numbers:numbers
+
+
+ }
+ return passwordChoice
+
+} 
+
+function randomChoice(array){
+  var randomE = array [randomI];
+  var randomI = Math.floor(Math.random()* array.length)
+  return randomE
+}
+
+function generatePassword() {
+  var choice = generatePasswordChoice();
+  var results = []
+  var pChar = []
+  var gChar = []
+  if (choice.lower) {
+    pChar = pChar.concat (lowerCase)
+    gChar.push(randomChoice(lowerCase))
+  }
+  if (choice.upper) {
+    pChar = pChar.concat (upperCase)
+    gChar.push(randomChoice(upperCase))
+  }
+  if (choice.symbols) {
+    pChar = pChar.concat (sChar)
+    gChar.push(randomChoice(sChar))
+  }
+  if (choice.numbers) {
+    pChar = pChar.concat (nChar)
+    gChar.push(randomChoice(nChar))
+  }
+  for (var i = 0; i < choice.length; i++){
+    var myChar = randomChoice(pChar)
+    results.push(myChar)
+  }
+  for (var i = 0; i < gChar.length; i++) {
+    results [i] = gChar [i]
+  }
+  return results.join("");
+  
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,30 +84,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Creating variables for the password criteria
-var length = 0;
-var charTypes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
-var charSymbols = ("!@#$%^&*?");
-
-
-// Generate password length
-function generatePassword() {
- 
-  length = prompt("How long do you want your password? Password MUST be between 8 and 128 characters");
-  if (length < 8 || length > 128){
-    alert("NO PASSWORD FOR YOU");
-    return null;
-  }
- return length;
-} 
-
-function generateCharType() {
-
-  charTypes = prompt("Password must contain Uppercase, Lowercase and Numerical characters, Still between 8 and 128 Characters");
-}
-
-
 
   // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
