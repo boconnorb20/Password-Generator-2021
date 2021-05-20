8b94a65141e0d1ac69edce29979432dddb8882e6
boconnorb20 <80371646+boconnorb20@users.noreply.github.com>
2021-05-20 16:57:46 -0500

    update js file

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,19 +9,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 // Creating variables for the password criteria
 var length = 0;
 var charTypes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
 var charSymbols = ("!@#$%^&*?");
 
+
 // Generate password length
-function generatePasswordLength() {
+function generatePassword() {
  
   length = prompt("How long do you want your password? Password MUST be between 8 and 128 characters");
   if (length < 8 || length > 128){
     alert("NO PASSWORD FOR YOU");
     return null;
- }
+  }
  return length;
 } 
 
